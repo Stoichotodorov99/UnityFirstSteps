cc1d041a4ffa10945ff3b0de4c119ef6020b6a35
Stoicho <stoicho.todorov9@gmail.com>
2025-01-10 23:07:29 +0200

    Added enemy prefab, different points on every enemy and scoreboard when player destroy them.

--- Galaxy Strike/Assets/Scripts/Enemy.cs
+++ Galaxy Strike/Assets/Scripts/Enemy.cs
@@ -4,9 +4,28 @@ public class Enemy : MonoBehaviour
 {
 
 [SerializeField] GameObject enemyDestroyVFX;
-private void OnParticleCollision(GameObject other) {
-    
-    Instantiate(enemyDestroyVFX, transform.position, Quaternion.identity);
-    Destroy(this.gameObject);
+[SerializeField] int hitPoint = 3;
+[SerializeField] int scoreValue = 10;
+Scoreboard scoreboard;
+
+private void Start() {
+    scoreboard = FindFirstObjectByType<Scoreboard>();
 }
+private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+
+    }
+
+    private void ProcessHit()
+    {
+        hitPoint--;
+        if (hitPoint <= 0)
+        {
+            scoreboard.IncreaseScore(scoreValue);
+            Instantiate(enemyDestroyVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+
+        }
+    }
 }
