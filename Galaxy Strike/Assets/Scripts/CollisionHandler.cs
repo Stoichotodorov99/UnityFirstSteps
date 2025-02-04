cb6daaf01c7abc49c46339d7c32428718939ab1e
Stoicho <stoicho.todorov9@gmail.com>
2025-02-05 00:36:13 +0200

    Added main game music, reload scene when player die, little post process optimization

--- Galaxy Strike/Assets/Scripts/CollisionHandler.cs
+++ Galaxy Strike/Assets/Scripts/CollisionHandler.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] GameObject playerDestroyVFX;
+      GameSceneManager gameSceneManager;
+    
+    private void Start() {
+        gameSceneManager = FindFirstObjectByType<GameSceneManager>();
+    }
 private void OnTriggerEnter(Collider other) {
 
+    gameSceneManager.ReloadLevel();
     Instantiate(playerDestroyVFX, transform.position, Quaternion.identity);
     Destroy(this.gameObject);
 
